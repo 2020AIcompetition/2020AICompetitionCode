9d55a74ea4fdea760f720150f89dfa74d30049ff
githubwqj2 <2812930196@qq.com>
2020-11-13 18:18:26 +0800

    添加解析内容

--- CrawlReadComprehension/ReadComprehension/spiders/kk_data_sources.py
+++ CrawlReadComprehension/ReadComprehension/spiders/kk_data_sources.py
@@ -30,7 +30,7 @@ class KkDataSourcesSpider(scrapy.Spider):
         self.log('Saved file %s.')  # self.log是运行日志，不是必要的
         if next_href is not None and len(next_href):  # 判断是否存在下一页
             next_page = response.urljoin(next_href[0])
-        yield scrapy.Request(next_page, callback=self.parse)
+        # yield scrapy.Request(next_page, callback=self.parse)
 
     # 解析文本数据
     def contentParse(self, response):
