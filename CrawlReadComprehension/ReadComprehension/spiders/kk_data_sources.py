8401c6b56b58ce1a599249e2aaf7a590bf9c182d
githubwqj2 <2812930196@qq.com>
2020-11-17 15:57:58 +0800

    添加解析内容

--- CrawlReadComprehension/ReadComprehension/spiders/kk_data_sources.py
+++ CrawlReadComprehension/ReadComprehension/spiders/kk_data_sources.py
@@ -2,7 +2,7 @@
 import time
 
 import scrapy
-
+import pymysql
 from CrawlReadComprehension.ReadComprehension.items import Economist
 
 
@@ -31,8 +31,14 @@ class KkDataSourcesSpider(scrapy.Spider):
             # paragraphs = contentResponse_2.xpath("//div[@class='qh_en']/p/text()")
             # print(paragraphs)
 
-            yield scrapy.Request(response.urljoin(contenturl), callback=self.contentParse,
-                                 meta={'chrome_flag': 0, "type": type, "title": title}, dont_filter=True)
+            # 判断该url是否已经存在库中,存在库中,则不进行爬取
+            if (contenturl.strip not in self.urlSet):
+                yield scrapy.Request(response.urljoin(contenturl), callback=self.contentParse,
+                                     meta={'chrome_flag': 0, "type": type, "title": title})
+            else:
+                self.repeat_url_number = self.repeat_url_number + 1
+                if (self.repeat_url_number > 50):
+                    self.crawler.engine.close_spider(self, "重复次数过多超过50次")
 
         next_href = response.xpath("//div[@class='page th']/a[text()='下一页']/@href").extract()
         self.log('Saved file %s.')  # self.log是运行日志，不是必要的
@@ -51,7 +57,7 @@ class KkDataSourcesSpider(scrapy.Spider):
 
         econo['content'] = "|||".join(paragraphs)
         econo['paragraphs'] = len(paragraphs)
-        econo['claw_url'] = response.url
+        econo['claw_url'] = response.url.strip
         yield econo
 
         # 可以使用lambda 函数
@@ -63,3 +69,21 @@ class KkDataSourcesSpider(scrapy.Spider):
         # def parse_page(self, response, pm, pv, dc):
         # print
         # pm, pv, dc
+
+    def __init__(self):
+        # 设置全局的重复url统计次数
+        self.repeat_url_number = 0;
+        # 建立连接
+        self.conn = pymysql.connect(host='115.159.151.166', user='root', passwd='123456', port=3306,
+                                    db='AIcourse')  # 有中文要存入数据库的话要加charset='utf8'
+        # 创建游标
+        self.cursor = self.conn.cursor()
+        select_sql = """
+                        select claw_url from  Economist
+                        """
+        self.cursor.execute(select_sql)
+        results = self.cursor.fetchall()
+        self.urlSet = set(str(url[0]).strip for url in results)
+        # urlSet=set(hash(url[0]).strip for url in results)
+        self.cursor.close()
+        self.conn.close()
